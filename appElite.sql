f6ad859ca52ff0cac3b2dba44d683ef5faef3871
Janneene <porpurina_irene@hotmail.com>
2022-07-04 22:36:27 -0400

    Create appElite.sql

---
+++ appElite.sql
@@ -0,0 +1,34 @@
+-- Geração de Modelo físico
+-- Sql ANSI 2003 - brModelo.
+
+
+
+CREATE TABLE tb_ficha (
+id_ficha integer PRIMARY KEY,
+forca integer,
+destreza integer,
+inteligencia integer,
+constituicao integer,
+carisma integer,
+acerto integer,
+acrobacia integer,
+carga integer,
+charme integer,
+concentracao integer,
+deslocamento integer,
+defesa integer,
+furtividade integer,
+intimidacao integer,
+investigacao integer,
+manipulacao integer,
+poder_bruto integer,
+reflexo integer,
+sorte integer,
+vida integer,
+vigor integer,
+raca text,
+classe text,
+idade integer,
+nivel integer,
+experiencia real
+)
