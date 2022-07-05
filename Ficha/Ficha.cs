e61f19ca7c4237dedcbe0371ffbe72d6ca7e322b
Janneene <porpurina_irene@hotmail.com>
2022-07-04 22:43:29 -0400

    Create Ficha.cs

---
+++ Ficha/Ficha.cs
@@ -0,0 +1,349 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Ficha
+{
+
+	int id_ficha;
+	string nome;
+	int def;
+	string raca;
+	string classe;
+	int idade;
+	string sexo;
+	int nivel;
+	float experiencia;
+	int vida;
+	int acerto;
+	int acrobacia;
+	int carga;
+	int charme;
+	int concentracao;
+	int furtividade;
+	int intimidacao;
+	int investigacao;
+	int manipulacao;
+	int poder_bruto;
+	int reflexo;
+	int sorte;
+	int vigor;
+	int deslocamento;
+	int forca;
+	int destreza;
+	int constituicao;
+	int inteligencia;
+	int carisma;
+	float peso;
+
+
+	public string exibir ()
+	{
+		return "Ficha{" + "id_ficha=" + id_ficha + ", nome=" + nome + ", def=" + def + ", raca=" + raca + ", classe=" + classe + ", idade=" + idade + ", sexo=" + sexo + ", nivel=" + nivel + ", experiencia=" + experiencia + ", vida=" + vida + ", acerto=" + acerto + ", acrobacia=" + acrobacia + ", carga=" + carga + ", charme=" + charme + ", concentracao=" + concentracao + ", furtividade=" + furtividade + ", intimidacao=" + intimidacao + ", investigacao=" + investigacao + ", manipulacao=" + manipulacao + ", poder_bruto=" + poder_bruto + ", reflexo=" + reflexo + ", sorte=" + sorte + ", vigor=" + vigor + ", deslocamento=" + deslocamento + ", forca=" + forca + ", destreza=" + destreza + ", constituicao=" + constituicao + ", inteligencia=" + inteligencia + ", carisma=" + carisma + ", peso=" + peso + '}';
+	}
+
+	public int getId_ficha ()
+	{
+		return id_ficha;
+	}
+
+	public void setId_ficha (int id_ficha)
+	{
+		this.id_ficha = id_ficha;
+	}
+
+	public string getNome ()
+	{
+		return nome;
+	}
+
+	public void setNome (string nome)
+	{
+		this.nome = nome;
+	}
+
+	public int getDef ()
+	{
+		return def;
+	}
+
+	public void setDef (int def)
+	{
+		this.def = def;
+	}
+
+	public string getRaca ()
+	{
+		return raca;
+	}
+
+	public void setRaca (string raca)
+	{
+		this.raca = raca;
+	}
+
+	public string getClasse ()
+	{
+		return classe;
+	}
+
+	public void setClasse (string classe)
+	{
+		this.classe = classe;
+	}
+
+	public int getIdade ()
+	{
+		return idade;
+	}
+
+	public void setIdade (int idade)
+	{
+		this.idade = idade;
+	}
+
+	public string getSexo ()
+	{
+		return sexo;
+	}
+
+	public void setSexo (string sexo)
+	{
+		this.sexo = sexo;
+	}
+
+	public int getNivel ()
+	{
+		return nivel;
+	}
+
+	public void setNivel (int nivel)
+	{
+		this.nivel = nivel;
+	}
+
+	public float getExperiencia ()
+	{
+		return experiencia;
+	}
+
+	public void setExperiencia (float experiencia)
+	{
+		this.experiencia = experiencia;
+	}
+
+	public int getVida ()
+	{
+		return vida;
+	}
+
+	public void setVida (int vida)
+	{
+		this.vida = vida;
+	}
+
+	public int getAcerto ()
+	{
+		return acerto;
+	}
+
+	public void setAcerto (int acerto)
+	{
+		this.acerto = acerto;
+	}
+
+	public int getAcrobacia ()
+	{
+		return acrobacia;
+	}
+
+	public void setAcrobacia (int acrobacia)
+	{
+		this.acrobacia = acrobacia;
+	}
+
+	public int getCarga ()
+	{
+		return carga;
+	}
+
+	public void setCarga (int carga)
+	{
+		this.carga = carga;
+	}
+
+	public int getCharme ()
+	{
+		return charme;
+	}
+
+	public void setCharme (int charme)
+	{
+		this.charme = charme;
+	}
+
+	public int getConcentracao ()
+	{
+		return concentracao;
+	}
+
+	public void setConcentracao (int concentracao)
+	{
+		this.concentracao = concentracao;
+	}
+
+	public int getFurtividade ()
+	{
+		return furtividade;
+	}
+
+	public void setFurtividade (int furtividade)
+	{
+		this.furtividade = furtividade;
+	}
+
+	public int getIntimidacao ()
+	{
+		return intimidacao;
+	}
+
+	public void setIntimidacao (int intimidacao)
+	{
+		this.intimidacao = intimidacao;
+	}
+
+	public int getInvestigacao ()
+	{
+		return investigacao;
+	}
+
+	public void setInvestigacao (int investigacao)
+	{
+		this.investigacao = investigacao;
+	}
+
+	public int getManipulacao ()
+	{
+		return manipulacao;
+	}
+
+	public void setManipulacao (int manipulacao)
+	{
+		this.manipulacao = manipulacao;
+	}
+
+	public int getPoder_bruto ()
+	{
+		return poder_bruto;
+	}
+
+	public void setPoder_bruto (int poder_bruto)
+	{
+		this.poder_bruto = poder_bruto;
+	}
+
+	public int getReflexo ()
+	{
+		return reflexo;
+	}
+
+	public void setReflexo (int reflexo)
+	{
+		this.reflexo = reflexo;
+	}
+
+	public int getSorte ()
+	{
+		return sorte;
+	}
+
+	public void setSorte (int sorte)
+	{
+		this.sorte = sorte;
+	}
+
+	public int getVigor ()
+	{
+		return vigor;
+	}
+
+	public void setVigor (int vigor)
+	{
+		this.vigor = vigor;
+	}
+
+	public int getDeslocamento ()
+	{
+		return deslocamento;
+	}
+
+	public void setDeslocamento (int deslocamento)
+	{
+		this.deslocamento = deslocamento;
+	}
+
+	public int getForca ()
+	{
+		return forca;
+	}
+
+	public void setForca (int forca)
+	{
+		this.forca = forca;
+	}
+
+	public int getDestreza ()
+	{
+		return destreza;
+	}
+
+	public void setDestreza (int destreza)
+	{
+		this.destreza = destreza;
+	}
+
+	public int getConstituicao ()
+	{
+		return constituicao;
+	}
+
+	public void setConstituicao (int constituicao)
+	{
+		this.constituicao = constituicao;
+	}
+
+	public int getInteligencia ()
+	{
+		return inteligencia;
+	}
+
+	public void setInteligencia (int inteligencia)
+	{
+		this.inteligencia = inteligencia;
+	}
+
+	public int getCarisma ()
+	{
+		return carisma;
+	}
+
+	public void setCarisma (int carisma)
+	{
+		this.carisma = carisma;
+	}
+
+	public float getPeso ()
+	{
+		return peso;
+	}
+
+	public void setPeso (float peso)
+	{
+		this.peso = peso;
+	}
+
+
+
+
+}
